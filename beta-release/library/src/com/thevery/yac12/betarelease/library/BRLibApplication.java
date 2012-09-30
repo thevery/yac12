7f7d82b13ed0a34fb3e5ee01a39a12d5c6612c2c
Ildar Karimov <thevery@yandex-team.ru>
2012-09-30 18:24:24 +0400

    initial commit with changing package name and label

---
+++ beta-release/library/src/com/thevery/yac12/betarelease/library/BRLibApplication.java
@@ -0,0 +1,13 @@
+package com.thevery.yac12.betarelease.library;
+
+import android.app.Application;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thevery
+ * Date: 30.09.12
+ * Time: 17:00
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class BRLibApplication extends Application {
+}
---
+++ beta-release/library/src/com/thevery/yac12/betarelease/library/MainActivity.java
@@ -0,0 +1,18 @@
+package com.thevery.yac12.betarelease.library;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thevery
+ * Date: 30.09.12
+ * Time: 17:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
